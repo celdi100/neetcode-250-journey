81b729cd3784175645e5127d248c637e72e81dc3
Abdul <aabdulmajeed.isa@gmail.com>
2025-04-18 08:33:05 +0100

    Fix up code

---
+++ solutions/01_arrays_and_hashing/01_Concatenation_of_Array.py
@@ -0,0 +1,29 @@
+"""
+Problem: [Concatenation of Array]
+Difficulty: [Easy]
+LeetCode #: [1929]
+NeetCode #: [1/250]
+Date Solved: [17/04/25]
+Attempt: [First Attempt]
+
+Problem Description:
+-
+
+Examples:
+-
+
+Approach:
+-
+
+Time Complexity: O(n)
+Space Complexity: O(n)
+
+Key Insights:
+Very easy
+
+# Revision Log
+"""
+
+def getConcatenation(nums):
+    return nums + nums
+    
